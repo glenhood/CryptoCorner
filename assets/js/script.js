5c9a1a510a57adeab2e81edfe85af0f0d9da8f26
Glen Hood <glenhoodatx@gmail.com>
2021-07-08 15:08:30 -0500

    created addtion buttons to be assigned

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,54 @@
-
+// bitcoin button
 var bitcoinButton = document.querySelector(".bitcoin-button")
 
+
 bitcoinButton.addEventListener("click", function (event) {
+console.log(event)
+
+var crypto = 'bitcoin';
+
+var redirectUrl = ("./indexCoinSelected.html")
+
+document.location.replace(redirectUrl)
+
+})
+// ethereum
+var ethereumButton = document.querySelector(".ethereum-button")
+
+ethereumButton.addEventListener("click", function (event) {
+console.log(event)
+
+var redirectUrl = ("./indexCoinSelected.html")
+
+document.location.replace(redirectUrl)
+
+})
+// tether button
+var tetherButton = document.querySelector(".tether-button")
+
+tetherButton.addEventListener("click", function (event) {
+console.log(event)
+
+var redirectUrl = ("./indexCoinSelected.html")
+
+document.location.replace(redirectUrl)
+
+})
+// binance coin button
+var binanceButton = document.querySelector(".binance-button")
+
+binanceButton.addEventListener("click", function (event) {
+console.log(event)
+
+var redirectUrl = ("./indexCoinSelected.html")
+
+document.location.replace(redirectUrl)
+
+})
+// doge button
+var dogeButton = document.querySelector(".doge-button")
+
+dogeButton.addEventListener("click", function (event) {
 console.log(event)
 
 var redirectUrl = ("./indexCoinSelected.html")
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,5 +1,8 @@
+
+
 var newsEl = document.querySelector('.news');
 // var urlLink = document.getElementById('url');
+var tickerEl = document.querySelector('.ticker');
 
 
 fetch("https://salty-mountain-68764.herokuapp.com/https://newsdata.io/api/1/news?apikey=pub_486ab7a2d3cb5f723e5c9de7e68dac7e0ba&q=bitcoin&language=en")
@@ -11,12 +14,12 @@ fetch("https://salty-mountain-68764.herokuapp.com/https://newsdata.io/api/1/news
     console.log(data);
     // for (var i = 0; i < data.results.length; i++) {
        
-        var textEl = document.createElement('div');
-        textEl.textContent = data.results[i].title;
+        var textEl = document.querySelector('#one-one');
+        textEl.textContent = data.results[0].title;
         newsEl.append(textEl)
 
-        var urlEl = document.createElement('div');
-        urlEl.textContent = data.results[i].link;
+        var urlEl = document.querySelector('#one-two');
+        urlEl.textContent = data.results[0].link;
         newsEl.append(urlEl);
     
 });
